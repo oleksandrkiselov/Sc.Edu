c15fa46fb3ccf43225fc4ffed834b87b54bf9d6d
Oleksandr Kiselov <omkiselov@gmail.com>
2021-12-16 12:27:37 +0200

    Carousel added

---
+++ Sc.Edu.Web/Controllers/CarouselController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Sc.Edu.Web.Models;
+using Sitecore.Data.Fields;
+using Sitecore.Mvc.Presentation;
+using Sitecore.Web.UI.WebControls;
+
+namespace Sc.Edu.Web.Controllers
+{
+    public class CarouselController : Controller
+    {
+        // GET: Carousel
+        public ActionResult Index()
+        {
+            var model = new CarouselModel();
+            List<Slide> slides = new List<Slide>();
+            var dataSource = RenderingContext.Current?.Rendering.Item;
+
+            MultilistField slidesField = dataSource.Fields["Slides"];
+
+            if (slidesField.Count > 0)
+            {
+                var slideItems = slidesField.GetItems();
+                foreach (var slideItem in slideItems)
+                {
+                    var titleField = slideItem.Fields["Title"];
+                    var title = titleField.Value;
+
+                    var subTitle = new MvcHtmlString(FieldRenderer.Render(slideItem,
+                            "Sub_title"));
+
+                    var image = new MvcHtmlString(FieldRenderer.Render(slideItem,
+                        "Image"));
+
+                    var callToAction = new MvcHtmlString(FieldRenderer.Render(slideItem,
+                        "Call_To_Action", "class=btn animated fadeInUp" ));
+
+                    slides.Add(new Slide
+                    {
+                        Title =title,
+                        SubTitle = subTitle,
+                        Image = image,
+                        CallToAction = callToAction
+                    });
+                }
+
+                model.Slides = slides;
+            }
+            return View(model);
+        }
+    }
+}
